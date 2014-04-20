0ee097339c7c5b97acf7555b01780018ebfaec2f
Ilya Margolin <ilya@jimdo.com>
2014-04-20 22:50:37 +0200

    Works

--- lib/puppet/parser/functions/github_public_keys.rb
+++ lib/puppet/parser/functions/github_public_keys.rb
@@ -1,16 +1,14 @@
 require 'octokit'
 
-module GithubFunctions
-  def get_public_keys gh_user
-    raise Puppet::ParseError, "github_public_keys argument '#{gh_user}' does not look like username" unless gh_user =~ /[[:alnum:]-]+/
-    client = Octokit::Client.new
-    j = client.user_keys gh_user
-    j.map {|u| u['key']}
-  end
-end
 module Puppet::Parser::Functions
   newfunction(:github_public_keys, :type => :rvalue) do |args|
-    include GithubFunctions
-    args.map(get_public_keys).flatten
+    client = Octokit::Client.new
+    users_keys = args.map do |gh_user|
+      raise Puppet::ParseError, "github_public_keys argument '#{gh_user}' does not look like username" unless gh_user =~ /[[:alnum:]-]+/
+      j = client.user_keys gh_user
+      j = j.map{|u| u['key'] + " #{gh_user}(#{u['id']})"}
+      j.join "\n"
+    end
+    users_keys
   end
 end
